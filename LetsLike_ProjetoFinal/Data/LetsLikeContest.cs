9039877d702b3180087be723c3b78c197e0a7d01
João Estevan Barbosa <jestevan12@gmail.com>
2021-11-23 17:24:55 -0300

    Adicionar arquivos de projeto.

---
+++ LetsLike_ProjetoFinal/Data/LetsLikeContest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LetsLike_ProjetoFinal.Data
+{
+    public class LetsLikeContest : DbContext
+    {
+
+        public LetsLikeContest(DbContextOptions<LetsLikeContest> options : base(options)
+        {
+
+        } 
+    }
+}
